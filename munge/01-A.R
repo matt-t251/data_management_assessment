ce42ad9d09243736c593795ef5acf3fb786370e6
Matthew Taylor (PGT) <c3074667@newcastle.ac.uk>
2023-11-01 12:01:06 +0000

    munge compleate

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,15 @@
-# Example preprocessing script.
+# create a dataframe of learners, their ID, country, and detected country from all years
+appended_dataframe <- rbind(cyber.security.1_enrolments,
+                            cyber.security.2_enrolments,
+                            cyber.security.3_enrolments,
+                            cyber.security.4_enrolments,
+                            cyber.security.5_enrolments,
+                            cyber.security.6_enrolments,
+                            cyber.security.7_enrolments)
+
+# organisation_admin removed
+admin_removed <- subset(appended_dataframe, role != "organisation_admin")                         
+
+# remove access columns
+columns_to_keep <- c("learner_id", "enrolled_at", "role", "country", "detected_country")
+df_learners <- subset(admin_removed, select = columns_to_keep)
