b753d8d5aaa602d6d6f6b4caab7eee4f06caae38
Matthew Taylor <mattdt251@gmail.com>
2023-11-15 13:38:35 +0000

    V 1.0
    - report finalised
    - graphs added

--- src/barchart_country_engagement.R
+++ src/barchart_country_engagement.R
@@ -1,6 +1,6 @@
 ## INPUT: df_learners (02-A.R)
-##        df_country  (02_B.R)
-## OUTPUT: graphs/BarChartofCompletion.png
+##        df_country  (02-C.R)
+## OUTPUT: graphs/BarChartofEngagement.png
 
 library('ProjectTemplate')
 load.project()
@@ -22,14 +22,14 @@ df_for_barchart <- head(df_for_barchart, n=20)
 df_for_barchart$country <- countrycode(sourcevar = df_for_barchart$country, origin = "iso2c", destination = "country.name")
 
 # Create the bar chart
-barchart <- ggplot(df_for_barchart, aes(x = reorder(country, -completion_percent), y = completion_percent)) +
+barchart <- ggplot(df_for_barchart, aes(x = reorder(country, -engagement_percent), y = engagement_percent)) +
   geom_bar(stat = "identity", fill = "black") +
   labs(
-    title = "Bar Chart of Completion Percentage by Country",
+    title = "Bar Chart of Engagement Percentage by Country",
     x = "Country",
-    y = "Completion Percent"
+    y = "Engagement Percent"
   ) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggsave("H:/Documents/FutureLearn/graphs/BarChartofCompletion.png", plot = barchart, width = 6, height = 4, dpi = 300)
+ggsave("H:/Documents/FutureLearn/graphs/BarChartofEngagement.png", plot = barchart, width = 6, height = 4, dpi = 300)
